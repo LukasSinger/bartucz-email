c553e281f5ebc8764dab42fd41db1aaa391dcf4a
Lukas Singer <7627101@isd535.org>
2022-04-26 11:33:14 -0500

    Started email service

--- index.js
+++ index.js
@@ -1,19 +1,42 @@
 let http = require("http");
 let url = require("url");
 let fs = require("fs");
+let nodemailer = require("nodemailer");
+
+let transporter = nodemailer.createTransport({
+  host: "saturn.rochesterschools.org",
+  port: 25,
+  auth: {
+    user: process.env.USERNAME,
+    pass: process.env.PASSWORD
+  },
+  tls: {
+    rejectUnauthorized: false
+  }
+});
+let mailOptions = {
+  from: process.env.USERNAME,
+  to: process.env.BARTUCZ,
+  subject: "",
+  html: ""
+};
 
 http
   .createServer((req, res) => {
     let query = url.parse(req.url, true);
-    let file = query.pathname == "/" ? "index.html" : `.${query.pathname}`;
-    fs.readFile(file, (err, data) => {
-      if (err) {
-        res.writeHead(404, { "Content-Type": "text/html" });
-        return res.end("We couldn't find what you requested (404)");
-      }
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write(data);
-      return res.end();
-    });
+    if (query.query.subject) {
+
+    } else {
+      let file = query.pathname == "/" ? "index.html" : `.${query.pathname}`;
+      fs.readFile(file, (err, data) => {
+        if (err) {
+          res.writeHead(404, { "Content-Type": "text/html" });
+          return res.end("We couldn't find what you requested (404)");
+        }
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(data);
+        return res.end();
+      });
+    }
   })
   .listen(8080);
